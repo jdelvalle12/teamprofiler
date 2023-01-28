3224e794def0889664bb127886e7ad2af43688e7
Jose Del Valle <jdelvalle88@live.com>
2023-01-28 14:25:55 -0500

    adjusted codes in HTML and pasted HTML into index.js

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
-const  inquirer = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
-const employee = require('employee');
+const employee = require('./lib/employee');
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
 
-const generateHTML = ({name, ID, role, school, officeNumber, github, email}) =>
+const generateHTML = ({name, Id, role, school, officeNumber, github, email}) => {
 
-inquirer
+  `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Oxygen&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="./dist/style.css" />
+      <title>Team Players</title>
+  </head>
+  <body>
+      <header class="background-color-red text-align-center text-white">
+          <div id="header">  
+              <a href="index.html" class="logo">Team Players</a>
+          </div>
+      </header>
+  
+      <section id="container background-color-blue text-align-center text-white">
+          <div class="manager">
+          <div class="col-12 col-md-4">
+          <div class="card">
+          <h1 class="card-header" input="name">${name}</h1>
+          <h3 input="role">${role}</h3>
+          <p input="id">${Id}</p>
+          <p input="office number">${officeNumber}</p>
+          </div>
+          </div>
+          </div>
+      </section>
+  
+      <section id="container background-color-blue text-align-center text-white">
+          <div class="engineer">
+          <div class="col-12 col-md-4">
+          <div class="card">
+          <h1 class="card-header" input="name">${name}</h1>
+          <h3 input="role">${role}</h3>
+          <p input="id">${id}</p>
+          <p input="email">${email}</p>
+          <p input="github">${github}</p>
+          </div>
+          </div>
+          </div>
+      </section>
+  
+      <section id="container background-color-blue text-align-center text-white">
+          <div class="intern">
+          <div class="col-12 col-md-4">
+          <div class="card">
+          <h1 class="card-header" input="name">${name}</h1>
+          <h3 input="role">${role}</h3>
+          <p input="id">${id}</p>
+          <p input="email">${email}</p>
+          <p input="school">${school}</p>
+          </div>
+          </div>
+          </div>
+      </section>
+  </body>
+  </html>`
+
+const manager = () => {
+  return inquirer
     .prompt ([
           {
             type: 'input',
             name: 'name',
-            message: 'What is the employee name?',
+            message: 'What is the manager name?',
           },
           {
             type: 'input',
-            message: 'What is the employee ID?',
-            name: 'ID',
+            message: 'What is the manager Id?',
+            name: 'Id',
           },
           {
             type: 'input',
@@ -23,31 +90,20 @@ inquirer
           },
           {
             type: 'input',
-            name: 'school',
-            message: 'What school is the employee attend?',
-          },
-          {
-            type: 'input',
             name: 'office number',
-            message: 'What is the office number?',
-          },
-          {
-            type: 'input',
-            name: 'github',
-            message: 'What is the employee GitHub link?',
-          },
-          {
-            type: 'input',
-            name: 'email',
-            message: 'What is the employee email address?',
+            message: 'What is the manager office number?',
           },
       ])
     .then((answers) => {
     const htmlPageContent = generateHTML(answers);
     fs.writeFile('index.html', htmlPageContent, (err) =>
     err ? console.log(err) : console.log('Successfully created index.html!')
-  );
-});
+      );
+    });
+
+  }
+};
+
 
 
 // TODO: Create a function to initialize app
--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,7 +1,14 @@
-const Employee = require('./lib/employee');
+const Employee = require('../lib/employee');
 
-describe('Employee class', () => {
+describe('Employee', () => {
     describe('getName method', () => {
-
+        it("should get the employee's name", () => {
+            const employee = new Employee('Jose', 106512, 'jdelvalle88@live.com');
+      
+            // Verify that the new object has the correct properties
+            expect(employee.name).toEqual('Jose');
+            expect(employee.id).toEqual(106512);
+            expect(employee.email).toEqual('jdelvalle88@live.com');
+          });
     })
 })
